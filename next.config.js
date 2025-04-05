0beeba04f8da4207c5008e130e888bbe64729827
Jérémy Tarlié <contact@tarlie.fr>
2025-04-05 15:14:45 +0200

    front

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
   webpack: (config) => {
     return config; // Force l'utilisation de Webpack au lieu de Turbopack
   },
+  env: {
+    NEXT_PUBLIC_TURNSTILE_SITE_KEY: process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY, // Ajout de la variable d'environnement
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
